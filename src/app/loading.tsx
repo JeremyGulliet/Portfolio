b88a60976639b9ec315408d933cd11794af4ae43
Jeremy GULLIET <jrmy.gulliet@gmail.com>
2024-11-18 17:47:50 +0100

    Maj loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900 dark:border-white"></div>
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-50">
+      <div className="h-12 w-12 animate-spin rounded-full border-t-4 border-pink-600" />
     </div>
   );
 }
